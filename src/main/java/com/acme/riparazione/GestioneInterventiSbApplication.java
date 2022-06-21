fac9a194544aecbf6a84786ccd2ea09d972f106b
Albert <47365826+os3albert@users.noreply.github.com>
2022-06-21 14:32:54 +0200

    first commit

---
+++ src/main/java/com/acme/riparazione/GestioneInterventiSbApplication.java
@@ -0,0 +1,13 @@
+package com.acme.riparazione;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneInterventiSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneInterventiSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acme/riparazione/GestioneInterventiSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.riparazione;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneInterventiSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
