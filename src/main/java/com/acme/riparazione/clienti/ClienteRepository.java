d564a402321ca11edbb04d2931fa213fee574b4a
david <david@DESKTOP-499U3MH.mshome.net>
2022-06-27 16:16:56 +0200

    davide rapportini 27.06 nuovo

--- src/main/java/com/acme/riparazione/clienti/ClienteRepository.java
+++ src/main/java/com/acme/riparazione/clienti/ClienteRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 @Repository
 public interface ClienteRepository extends PagingAndSortingRepository<ClienteImp, Long> {
-
+	
 }
